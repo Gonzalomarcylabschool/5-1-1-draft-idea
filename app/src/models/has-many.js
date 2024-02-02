719d24fc4cfeb903859e6b82a595301a7f8d8f7a
gonzalorportfolio <gromeroeducation@gmail.com>
2024-02-02 12:00:56 -0500

    update

--- app/src/models/has-many.js
+++ app/src/models/has-many.js
@@ -1,15 +1,30 @@
 import getId from "../utils/getId.js";
+import Book from "./belongs-to.js";
 
 // build the class that would have many things
 export class Author {
+  #books
+  static #allAuthors = []; 
   constructor(name) {
       this.id = getId();
       this.name = name;
-      this.books = []; // Array to store books
-  }
+      this.#books = []; // Array to store books
+      Author.#allAuthors.push(this);
+  };
 
-  // Method to add a book to the author's collection
-  addBook(book) {
-      this.books.push(book);
-  }
+  addBook(title, authorId) {
+    const author = Author.#allAuthors.find((author) => author.id === authorId);
+    if (author) {
+      const newBook = new Book(title, authorId);
+      author.#books.push(newBook);
+      return newBook;
+    } else {
+      console.error(`Author with ID ${authorId} not found.`);
+      return null;
+    }
+  };
+
+  getBooks(){
+    return [...this.#books];
+  };
 }
